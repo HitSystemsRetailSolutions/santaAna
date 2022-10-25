3f4998c152ceb57e06018193043df1654bf3a0eb
Ezequiel <ezequielucho@gmail.com>
2022-10-25 11:53:13 +0200

    Update mongodb.ts

--- src/conexion/mongodb.ts
+++ src/conexion/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 import { MONGO_USER, MONGO_PASSWORD } from "src/secrets";
-const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@34.78.247.153:27017/dbname?authSource=solucionesit365`;
+const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:27017/dbname?authSource=solucionesit365`;
 
 const client = new MongoClient(uri);
 const conexion = client.connect();
