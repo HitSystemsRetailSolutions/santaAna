261cc3088bd77b12b99358748d56e4eca7e6004e
Ezequiel <ezequielucho@gmail.com>
2022-10-25 12:15:00 +0200

    getPromocionesNueva
    Solo para Toc Game V4 con Auth

--- src/promociones/promociones.class.ts
+++ src/promociones/promociones.class.ts
@@ -2,57 +2,109 @@ import { IResult } from "mssql";
 import { recHit } from "src/conexion/mssql";
 
 export class PromocionesClass {
-    getPromocionesUgly(database: string, codigoCliente: number) {
-        const sql = `SELECT Id as _id, Di as fechaInicio, Df as fechaFinal, D_Producte as principal, D_Quantitat as cantidadPrincipal, S_Producte as secundario, S_Quantitat as cantidadSecundario, S_Preu as precioFinal FROM ProductesPromocionats WHERE Client = ${codigoCliente}`;
-        return recHit(database, sql).then((res: IResult<any>) => {
-            if (codigoCliente == 842) {
-                console.log("La realidad: ", res);
-            }
-            
-            if (res) {
-                if (res.recordset.length > 0) {
-                    return res.recordset;
-                }
-            }
-            return  [];
-        }).catch((res) => {
-            console.log(res);
-            return [];
-        });
+  getPromocionesUgly(database: string, codigoCliente: number) {
+    const sql = `SELECT Id as _id, Di as fechaInicio, Df as fechaFinal, D_Producte as principal, D_Quantitat as cantidadPrincipal, S_Producte as secundario, S_Quantitat as cantidadSecundario, S_Preu as precioFinal FROM ProductesPromocionats WHERE Client = ${codigoCliente}`;
+    return recHit(database, sql)
+      .then((res: IResult<any>) => {
+        if (codigoCliente == 842) {
+          console.log("La realidad: ", res);
+        }
+
+        if (res) {
+          if (res.recordset.length > 0) {
+            return res.recordset;
+          }
+        }
+        return [];
+      })
+      .catch((res) => {
+        console.log(res);
+        return [];
+      });
+  }
+
+  /* Para tocGamev4 */
+  async getPromocionesNueva(database: string, codigoCliente: number) {
+    let objPrincipal = null;
+    let objSecundario = null;
+    let promociones = [];
+
+    promociones = await this.getPromocionesUgly(database, codigoCliente);
+
+    for (let i = 0; i < promociones.length; i++) {
+      if (promociones[i].principal.startsWith("F_")) {
+        objPrincipal = await recHit(
+          database,
+          `select Codi as _id from articles where familia = '${promociones[
+            i
+          ].principal.substring(2)}'`
+        );
+        promociones[i].principal = objPrincipal.recordset;
+      } else {
+        promociones[i].principal = [Number(promociones[i].principal)];
+      }
+
+      if (promociones[i].secundario.startsWith("F_")) {
+        objSecundario = await recHit(
+          database,
+          `select Codi as _id from articles where familia = '${promociones[
+            i
+          ].secundario.substring(2)}'`
+        );
+        promociones[i].secundario = objSecundario.recordset;
+      } else {
+        promociones[i].secundario = [Number(promociones[i].secundario)];
+      }
+      objPrincipal = null;
+      objSecundario = null;
     }
 
-    async getPromociones(database: string, codigoCliente: number) {
-        let objPrincipal = null;
-        let objSecundario = null;
-        let promociones = null;
+    return promociones;
+  }
 
-        try {
-            promociones = await this.getPromocionesUgly(database, codigoCliente);
-        }
-        catch(err) {
-            console.log(err);
-            return [];
-        }
-        
-        for(let i = 0; i < promociones.length; i++) {
-            if(promociones[i].principal.startsWith('F_')) {
-                objPrincipal = await recHit(database, `select Codi as _id from articles where familia = '${promociones[i].principal.substring(2)}'`);
-                promociones[i].principal = objPrincipal.recordset;
-            } else {
-                promociones[i].principal = [{_id: Number(promociones[i].principal)}]
-            }
+  async getPromociones(database: string, codigoCliente: number) {
+    let objPrincipal = null;
+    let objSecundario = null;
+    let promociones = null;
 
-            if(promociones[i].secundario.startsWith('F_')) {
-                objSecundario = await recHit(database, `select Codi as _id from articles where familia = '${promociones[i].secundario.substring(2)}'`);
-                promociones[i].secundario = objSecundario.recordset;
-            } else {
-                promociones[i].secundario = [{_id: Number(promociones[i].secundario)}]
-            }
-            objPrincipal    = null;
-            objSecundario   = null;
-        }
-
-        return promociones;
+    try {
+      promociones = await this.getPromocionesUgly(database, codigoCliente);
+    } catch (err) {
+      console.log(err);
+      return [];
     }
+
+    for (let i = 0; i < promociones.length; i++) {
+      if (promociones[i].principal.startsWith("F_")) {
+        objPrincipal = await recHit(
+          database,
+          `select Codi as _id from articles where familia = '${promociones[
+            i
+          ].principal.substring(2)}'`
+        );
+        promociones[i].principal = objPrincipal.recordset;
+      } else {
+        promociones[i].principal = [{ _id: Number(promociones[i].principal) }];
+      }
+
+      if (promociones[i].secundario.startsWith("F_")) {
+        objSecundario = await recHit(
+          database,
+          `select Codi as _id from articles where familia = '${promociones[
+            i
+          ].secundario.substring(2)}'`
+        );
+        promociones[i].secundario = objSecundario.recordset;
+      } else {
+        promociones[i].secundario = [
+          { _id: Number(promociones[i].secundario) },
+        ];
+      }
+      objPrincipal = null;
+      objSecundario = null;
+    }
+
+    return promociones;
+  }
 }
 export const promocionesInstance = new PromocionesClass();
--- src/promociones/promociones.controller.ts
+++ src/promociones/promociones.controller.ts
@@ -1,23 +1,59 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { promocionesInstance } from './promociones.class';
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { Request } from "express";
+import { authInstance } from "src/auth/auth.class";
+import { promocionesInstance } from "./promociones.class";
 
-@Controller('promociones')
+@Controller("promociones")
 export class PromocionesController {
-    @Post('getPromociones')
-    getPromociones(@Body() params) {
-        if (params != null && params != undefined) {
-            if (params.database != null && params.database != undefined && params.codigoTienda != null && params.codigoTienda != undefined) {
-                return promocionesInstance.getPromociones(params.database, params.codigoTienda).then((promociones) => {
-                    return { error: false, info: promociones };
-                }).catch((err) => {
-                    console.log(err);
-                    return { error: true, mensaje: 'SanPedro: Faltan datos promociones/getPromociones > getPromociones() CATCH' };
-                });
-            } else {
-                return { error: true, mensaje: 'SanPedro: Faltan datos promociones/getPromociones' };
-            }
-        } else {
-            return { error: true, mensaje: 'SanPedro: Faltan todos los datos promociones/getPromociones' };
-        }
+  @Post("getPromociones")
+  getPromociones(@Body() params) {
+    if (params != null && params != undefined) {
+      if (
+        params.database != null &&
+        params.database != undefined &&
+        params.codigoTienda != null &&
+        params.codigoTienda != undefined
+      ) {
+        return promocionesInstance
+          .getPromociones(params.database, params.codigoTienda)
+          .then((promociones) => {
+            return { error: false, info: promociones };
+          })
+          .catch((err) => {
+            console.log(err);
+            return {
+              error: true,
+              mensaje:
+                "SanPedro: Faltan datos promociones/getPromociones > getPromociones() CATCH",
+            };
+          });
+      } else {
+        return {
+          error: true,
+          mensaje: "SanPedro: Faltan datos promociones/getPromociones",
+        };
+      }
+    } else {
+      return {
+        error: true,
+        mensaje: "SanPedro: Faltan todos los datos promociones/getPromociones",
+      };
     }
+  }
+
+  /* Para tocGameV4 */
+  @Get("getPromocionesNueva")
+  async getPromocionesNueva(@Req() req: Request) {
+    try {
+        const token = req.headers.authorization;
+        if (token) {
+            const parametros = await authInstance.getParametros(token);
+            return promocionesInstance.getPromocionesNueva(parametros.database, parametros.licencia);
+        }
+        throw Error("Falta autorización en getPromocionesNueva");
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+  }
 }
