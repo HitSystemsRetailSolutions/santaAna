7e445c53786770affc1e7b1818957c797e0a1d79
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-12-17 02:08:11 +0100

    Update movimientos.class.ts

--- src/movimientos/movimientos.class.ts
+++ src/movimientos/movimientos.class.ts
@@ -73,11 +73,6 @@ function comprobarMovimiento(movimiento: MovimientosInterface) {
         mensaje += 'idTrabajador incorrecto\n';
     }
 
-    if (movimiento.tipoExtra == null || movimiento.tipoExtra == undefined) {
-        error = true;
-        mensaje += 'tipoExtra incorrecto\n';
-    }
-
     if (movimiento.idTicket == null || movimiento.idTicket == undefined) {
         error = true;
         mensaje += 'idTicket incorrecto\n';
@@ -108,7 +103,7 @@ class Movimientos {
                 let concepto = movimiento.concepto;
 
                 if(movimiento.idTicket != -100) {
-                    switch(movimiento.tipoExtra) {
+                    switch(movimiento.tipo) {
                         case 'TARJETA': concepto = `Pagat Targeta: ${movimiento.idTicket}`; break;
                         case 'TKRS_CON_EXCESO': 
                             concepto = `Excs.TkRs:  [${movimiento.idTicket}]`;
@@ -116,59 +111,58 @@ class Movimientos {
                         case 'TKRS_SIN_EXCESO': 
                             concepto = `Pagat TkRs:  [${movimiento.idTicket}]`;
                             break;
-                        case 'CONSUMO_PERSONAL': break;
                         case 'DEUDA': break; //el concepto de deuda se controla desde el tocGame
                         default: error = true; break;
                     }                   
                 }
 
-                if (!error) {
-                    if(movimiento.codigoBarras != "" && movimiento.codigoBarras != undefined) {
-                        sqlBarras = `INSERT INTO CodisBarresReferencies (Num, Tipus, Estat, Data, TmSt, Param1, Param2, Param3, Param4) VALUES (${movimiento.codigoBarras}, 'Moviments', 'Creat', CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120), CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120), ${parametros.licencia}, ${movimiento.idTrabajador}, ${-movimiento.valor}, '${fecha.day}/${fecha.month}/${fecha.year} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}');`;
-                    }
-                    sql = `
-                    IF EXISTS (select  * from ${nombreTabla} WHERE botiga = ${parametros.codigoTienda} AND Import = ${(movimiento.tipo == "SALIDA") ? -movimiento.valor : movimiento.valor} AND Tipus_moviment = '${(movimiento.tipo == "SALIDA") ? 'O':'A'}' AND Data = CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120))
-                        BEGIN
-                            SELECT 'YA_EXISTE' as resultado
-                        END
-                    ELSE
-                        BEGIN
-                            DELETE FROM ${nombreTabla} WHERE Botiga = ${parametros.codigoTienda} AND Data = CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120) AND  Dependenta = ${movimiento.idTrabajador} AND Import = ${(movimiento.tipo == "SALIDA") ? -movimiento.valor : movimiento.valor};
-                            INSERT INTO ${nombreTabla} (Botiga, Data, Dependenta, Tipus_moviment, Import, Motiu) VALUES (${parametros.codigoTienda}, CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120), ${movimiento.idTrabajador}, '${(movimiento.tipo == "SALIDA") ? 'O':'A'}', ${(movimiento.tipo == "SALIDA") ? -movimiento.valor : movimiento.valor}, '${limpiarStringCaracteresEspeciales(concepto)}'); 
-                            ${sqlBarras} 
-                            SELECT 'OK' as resultado
-                        END
-                    `;
+                // if (!error) {   COMENTADO POR PRUEBAS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //     if(movimiento.codigoBarras != "" && movimiento.codigoBarras != undefined) {
+                //         sqlBarras = `INSERT INTO CodisBarresReferencies (Num, Tipus, Estat, Data, TmSt, Param1, Param2, Param3, Param4) VALUES (${movimiento.codigoBarras}, 'Moviments', 'Creat', CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120), CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120), ${parametros.licencia}, ${movimiento.idTrabajador}, ${-movimiento.valor}, '${fecha.day}/${fecha.month}/${fecha.year} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}');`;
+                //     }
+                //     sql = `
+                //     IF EXISTS (select  * from ${nombreTabla} WHERE botiga = ${parametros.codigoTienda} AND Import = ${(movimiento.tipo == "SALIDA") ? -movimiento.valor : movimiento.valor} AND Tipus_moviment = '${(movimiento.tipo == "SALIDA") ? 'O':'A'}' AND Data = CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120))
+                //         BEGIN
+                //             SELECT 'YA_EXISTE' as resultado
+                //         END
+                //     ELSE
+                //         BEGIN
+                //             DELETE FROM ${nombreTabla} WHERE Botiga = ${parametros.codigoTienda} AND Data = CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120) AND  Dependenta = ${movimiento.idTrabajador} AND Import = ${(movimiento.tipo == "SALIDA") ? -movimiento.valor : movimiento.valor};
+                //             INSERT INTO ${nombreTabla} (Botiga, Data, Dependenta, Tipus_moviment, Import, Motiu) VALUES (${parametros.codigoTienda}, CONVERT(datetime, '${fecha.year}-${fecha.month}-${fecha.day} ${fecha.hours}:${fecha.minutes}:${fecha.seconds}', 120), ${movimiento.idTrabajador}, '${(movimiento.tipo == "SALIDA") ? 'O':'A'}', ${(movimiento.tipo == "SALIDA") ? -movimiento.valor : movimiento.valor}, '${limpiarStringCaracteresEspeciales(concepto)}'); 
+                //             ${sqlBarras} 
+                //             SELECT 'OK' as resultado
+                //         END
+                //     `;
                     
-                    /* Controlador de intentos nulo o indefinido */
-                    if (movimiento.intentos == null || movimiento.intentos == undefined) {
-                        movimiento.intentos = 1;
-                    } else {
-                        movimiento.intentos += 1;
-                    }
-                    /* Controlador para reiniciar comentario */
-                    movimiento["comentario"] = '';
+                //     /* Controlador de intentos nulo o indefinido */
+                //     if (movimiento.intentos == null || movimiento.intentos == undefined) {
+                //         movimiento.intentos = 1;
+                //     } else {
+                //         movimiento.intentos += 1;
+                //     }
+                //     /* Controlador para reiniciar comentario */
+                //     movimiento["comentario"] = '';
                     
-                    recHit(parametros.database, sql).then((res) => {
-                        if (res.recordset[0].resultado == 'OK' && res.rowsAffected.length > 0) {
-                            movimiento.enviado = true;
-                            server.emit('resMovimientos', { error: false, movimiento });
-                        } else if (res.recordset[0].resultado == 'YA_EXISTE') {
-                            movimiento.enviado = true;
-                            server.emit('resMovimientos', { error: false, movimiento });
-                        } else {
-                            movimiento["comentario"] = 'Error desconocido';
-                            server.emit('resMovimientos', { error: true,  movimiento, mensaje : 'Error desconocido'});
-                        }
-                    }).catch((err) => {
-                        console.log(err);
-                        movimiento["comentario"] = 'Error en la consulta SQL 1: ' + sql;
-                        server.emit('resMovimientos', { error: true,  movimiento, mensaje : 'Error en SQL 2: ' + sql});
-                    });
-                } else {
-                    movimiento["comentario"] = 'Error en tipoExtra';
-                    server.emit('resMovimientos', { error: true, movimiento, mensaje: 'Error en tipoExtra: ' + movimiento.tipoExtra });
-                }
+                //     recHit(parametros.database, sql).then((res) => {
+                //         if (res.recordset[0].resultado == 'OK' && res.rowsAffected.length > 0) {
+                //             movimiento.enviado = true;
+                //             server.emit('resMovimientos', { error: false, movimiento });
+                //         } else if (res.recordset[0].resultado == 'YA_EXISTE') {
+                //             movimiento.enviado = true;
+                //             server.emit('resMovimientos', { error: false, movimiento });
+                //         } else {
+                //             movimiento["comentario"] = 'Error desconocido';
+                //             server.emit('resMovimientos', { error: true,  movimiento, mensaje : 'Error desconocido'});
+                //         }
+                //     }).catch((err) => {
+                //         console.log(err);
+                //         movimiento["comentario"] = 'Error en la consulta SQL 1: ' + sql;
+                //         server.emit('resMovimientos', { error: true,  movimiento, mensaje : 'Error en SQL 2: ' + sql});
+                //     });
+                // } else {
+                //     movimiento["comentario"] = 'Error en tipoExtra';
+                //     server.emit('resMovimientos', { error: true, movimiento, mensaje: 'Error en tipoExtra: ' + movimiento.tipoExtra });
+                // }
 
             } else {
                 movimiento["comentario"] = resMovimiento.mensaje;
