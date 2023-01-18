98e0eea4e8e03593d7f3184d7059948bcbb917dd
TecnicHIT <tecnic@hitsystems.es>
2023-01-18 12:36:55 +0100

    Cambio mongodb

--- src/conexion/mongodb.ts
+++ src/conexion/mongodb.ts
@@ -1,10 +1,10 @@
 import { MongoClient } from "mongodb";
 import { MONGO_USER, MONGO_PASSWORD, MONGO_SERVER } from "../secrets";
-const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_SERVER}:27017/dbname?authSource=solucionesit365`;
+const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_SERVER}:27017/dbname?authSource=santaAna`;
 
 const client = new MongoClient(uri);
 const conexion = client.connect();
 
 export async function getDb() {
-  return (await conexion).db("solucionesit365");
+  return (await conexion).db("santaAna");
 }
